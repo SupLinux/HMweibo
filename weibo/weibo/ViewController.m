833f618f8d4b1a1c92fe2055b13e57d4dbe78358
SupLinux <2501148856@qq.com>
2017-11-25 16:11:20 +0800

    第一次提交

---
+++ weibo/weibo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  weibo
+//
+//  Created by 姚群 on 2017/11/25.
+//  Copyright © 2017年 姚旭飚. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (nonatomic,assign) NSString* str;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
